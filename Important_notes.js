5ab79c1ac3ea7e9acbb567db6c96088e8c8e1790
Lasantha Rathnayake <lasanthakr77@gmail.com>
2017-12-06 17:18:38 -0500

    Notes 1

--- Important_notes.js
+++ Important_notes.js
@@ -1,4 +1,5 @@
 
+// Variables
 var name;
 
 name = "Jade";
@@ -8,3 +9,22 @@ document.write(name);
 prompt("What is your name")
 
 alert("Name is " + name)
+
+alert(name + 7) // Jade7
+
+
+// Operatos and expressions
+
+x = 12;
+
+x == "12"; // usually true. not check the type
+
+x === "12";  // false. Check the type
+
+x !== "12"; // True
+
+(15 > x) && (x>5) //true = and
+
+(15 > x) || (x>5) //true = or
+
+!(x == 12) // false = not
